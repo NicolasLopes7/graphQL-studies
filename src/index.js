3c822f5ed1a156dc8ffb1bf23177e34afeb44bc1
Nicolas Lopes Aquino <nicolaslopesbusiness@gmail.com>
2020-06-01 09:17:42 -0300

    Creating user type, and returning an example user in users query

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    author: User!
+  }
+
   type Query {
     hello: String
+    users: [User!]!
   }
 `;
+
 const resolvers = {
   Query: {
     hello: () => "Hello World",
+    users: () => [
+      {
+        _id: String(Math.random()),
+        name: "Nicolas",
+        email: "nicolaslopesaquino@gmail.com",
+        active: true,
+      },
+    ],
   },
 };
 
